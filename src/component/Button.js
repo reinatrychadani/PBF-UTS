ac4e75d5c7bcef05b8ff658fe96dc288338bc459
reinatrychadani <trycha67@gmail.com>
2020-04-02 13:30:50 +0700

    menghandle fungsi klik operator

---
+++ src/component/Button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+export default class Button extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    green: PropTypes.bool,
+    wide: PropTypes.bool,
+    clickHandler: PropTypes.func,
+  };
+
+  handleClick = () => {
+    this.props.clickHandler(this.props.name);
+  };
+
+  render() {
+    const className = [
+      "component-button",
+      this.props.green ? "green" : "",
+      this.props.wide ? "wide" : "",
+    ];
+
+    return (
+      <div className={className.join(" ").trim()}>
+        <button onClick={this.handleClick}>{this.props.name}</button>
+      </div>
+    );
+  }
+}
